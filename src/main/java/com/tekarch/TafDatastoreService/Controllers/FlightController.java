7b8efa1a1e0c5dc5d7ce8fcfc3ffbad192a83939
Sreedevi S <sreedevis@tekarch.com>
2025-01-21 20:49:35 -0800

    third commit

--- src/main/java/com/tekarch/TafDatastoreService/Controllers/FlightController.java
+++ src/main/java/com/tekarch/TafDatastoreService/Controllers/FlightController.java
@@ -4,10 +4,10 @@ import com.tekarch.TafDatastoreService.Models.Flight;
 import com.tekarch.TafDatastoreService.Service.FlightServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/datastore/flights")
@@ -23,6 +23,20 @@ public class FlightController {
         Flight addedFlight = flightService.addFlight(flight);
         return ResponseEntity.ok(addedFlight);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Flight>> getAllFlights(){
+        List<Flight> flights = flightService.getAllFlights();
+        return ResponseEntity.ok(flights);
+    }
+
+    @GetMapping("/{flightId}")
+    public ResponseEntity<Flight> getFlightById(@PathVariable Long flightId) {
+        Flight flight = flightService.getFlightById(flightId);
+        return flight != null ? ResponseEntity.ok(flight) : ResponseEntity.notFound().build();
+
+    }
+
 
 
 }
